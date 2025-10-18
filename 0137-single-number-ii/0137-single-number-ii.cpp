d05c2c01d285b33feea7bf22e93a18dff9235cd1
Vivek Kumar Mahto <vivekkumarmahto101103@gmail.com>
2025-10-18 07:10:24 +0530

    Time: 0 ms (100%), Space: 13.9 MB (20.31%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int>freq;
+        for(int i: nums) {
+            freq[i]++;
+        }
+        int ans = 0;
+        for(auto &p: freq) {
+            if(p.second > 0 && p.second < 2) {
+                ans = p.first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
