79612e2aea1fba2d1ea9b43e8984772b350e9ebc
Vivek Kumar Mahto <vivekkumarmahto101103@gmail.com>
2026-08-13 08:39:11 +0530

    Time: 7 ms (51.24%), Space: 13.3 MB (28.35%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -7,15 +7,6 @@ public:
         int size = s.size();
         int cnt = 0;
         int maxi = 0;
-        // for(int i = 0; i < size-k; i++) {
-        //     int cnt = 0;
-        //     for(int j = 0; j < k; j++) {
-        //         if(isVowel(s[i])) {
-        //             cnt++;
-        //             maxi = max(maxi, cnt);
-        //         }
-        //     }
-        // }
 
         for(int i = 0; i < k; i++) {
             if(isVowel(s[i])) {
